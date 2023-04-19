7222b248ee05ceaac90b836fd1eda939b7e10f33
Felipe Dalbert <felipeoficial195@gmail.com>
2023-04-18 23:05:36 -0300

    build(searchInput): adds new eventListenner

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -6,6 +6,8 @@ const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+const filterSelect = document.querySelector("#filter-select");
 
 let oldInputValue
 
@@ -115,4 +117,6 @@ editForm.addEventListener('submit', (e) =>{
     toggleForms()
 })
 
-
+searchInput.addEventListener('change', (e) =>{
+    
+})
